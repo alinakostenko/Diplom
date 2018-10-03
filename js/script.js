2d4f2db08efa9bdf657e33d51a2e12229b4a5ecd
akostenko <alinakostenko91@gmail.com>
2018-10-03 16:39:10 +0300

    done without media

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    //Slider
+
     $('#slider').slick({
         infinite: true,
         slidesToShow: 3,
@@ -9,6 +11,8 @@ $(document).ready(function(){
         nextArrow: $('.slick-prev'),
         variableWidth: true
     });
+
+    //Scrolling
 
     var $anchors = $('a[href^="#"]').not('[href="#"]');
 
@@ -22,11 +26,46 @@ $(document).ready(function(){
         }, 1000);
     });
 
+    //Isotope
+
     var $isotopeGrid = $('.saved-images');
 
     $isotopeGrid.isotope({
         itemSelector: 'img',
         percentPosition: true
+    });
+
+    //Modal
+    $('.inline1').modaal({
+        content_source: '#inline1'
+    });
+
+    $('.inline2').modaal({
+        content_source: '#inline2'
+    });
+
+    $('.inline3').modaal({
+        content_source: '#inline3'
+    });
+
+    $('.inline4').modaal({
+        content_source: '#inline4'
+    });
+
+    $('.inline5').modaal({
+        content_source: '#inline5'
+    });
+
+    $('.inline6').modaal({
+        content_source: '#inline6'
+    });
+
+    $('.inline7').modaal({
+        content_source: '#inline7'
+    });
+
+    $('.inline8').modaal({
+        content_source: '#inline8'
     });
 
 });
